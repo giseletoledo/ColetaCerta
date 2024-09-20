61595cce2af4d4b0d791c913e09b7d10d301adb8
Gisele <gisesonia@gmail.com>
2024-09-20 18:35:53 -0300

    feat: adicionando cores do tema light

--- app/src/main/java/br/com/androiddev/ui/theme/Color.kt
+++ app/src/main/java/br/com/androiddev/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Aqua = Color(0xFF20A49D)
+val AquaLight = Color(0xFF9AFFFA)
+val LightBlue = Color(0xFF78BAFF)
--- app/src/main/java/br/com/androiddev/ui/theme/Theme.kt
+++ app/src/main/java/br/com/androiddev/ui/theme/Theme.kt
@@ -18,9 +18,9 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Aqua,
+    secondary = AquaLight,
+    tertiary = LightBlue
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
