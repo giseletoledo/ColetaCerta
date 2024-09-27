fb0b914e14b15ff23aff0dc7f4342dab9fa03168
Gisele <gisesonia@gmail.com>
2024-09-27 13:54:18 -0300

    feat: tela principal

---
+++ app/src/main/java/br/com/androiddev/view/MainScreen.kt
@@ -0,0 +1,168 @@
+package br.com.androiddev.view
+
+import RoundedButton
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import br.com.androiddev.navigation.BottomNavigationBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(navController: NavController) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Coleta Certa") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(navController = navController)
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                RoundedButton(
+                    icon = Icons.Default.LocationOn,
+                    text = "Ecopontos",
+                    onClick = { navController.navigate("addressList") }
+                )
+                RoundedButton(
+                    icon = Icons.Default.Refresh,
+                    text = "Materiais",
+                    onClick = { /* Ação para Materiais */ }
+                )
+                RoundedButton(
+                    icon = Icons.Default.Delete,
+                    text = "Nova coleta",
+                    onClick = { /* Ação para Nova coleta */ }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                RoundedButton(
+                    icon = Icons.Default.Warning,
+                    text = "Manuseio",
+                    onClick = { /* Ação para Manuseio */ }
+                )
+                RoundedButton(
+                    icon = Icons.Default.Info,
+                    text = "Dicas",
+                    onClick = { /* Ação para Dicas */ }
+                )
+                RoundedButton(
+                    icon = Icons.Default.AccountBox,
+                    text = "Campanha",
+                    onClick = { /* Ação para Campanha */ }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Text(
+                text = "Você sabia?",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Create,
+                        contentDescription = "lápis",
+                        modifier = Modifier
+                            .size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "Desde 2020, com a implementação do Decreto n° 10.388, os consumidores podem descartar medicamentos vencidos ou em desuso em farmácias que possuem pontos de coleta.",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = "simbolo de informações",
+                        modifier = Modifier
+                            .size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "A reciclagem do óleo de cozinha usado permite a produção de sabão, biodiesel, tintas e outros produtos, contribuindo também para a redução da poluição ambiental.",
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
+    }
+}
+
